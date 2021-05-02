ac01f2c866077f4b3cd336658c949d9d26067495
JúDCraide <juliadcraide31@gmail.com>
2021-05-02 00:48:08 -0300

    refactor: identing filler companis const

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -6,7 +6,14 @@ import { Container } from './styles';
 
 export default function Landing() {
 
-  const companies=[{name:'Marca 1', score:2190},{name:'Marca 2', score:-3343},{name:'Marca 3', score:23242}]
+  const companies=[
+    {name:'Marca 1', score:2190},
+    {name:'Marca 2', score:-3343},
+    {name:'Marca 3', score:23242},
+    {name:'Marca 4', score:2190},
+    {name:'Marca 5', score:-3343},
+    {name:'Marca 6', score:23242}
+  ];
 
   return (
     <Container>
